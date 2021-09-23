c71ede398dbcb768db2efe17f643be1bcfc663d3
emnabz <1947@holbertonschool.com>
2021-09-23 16:56:56 +0100

    task0

---
+++ 0x0C-Javascript_advanced/0-welcome.js
@@ -0,0 +1,11 @@
+function welcome(firstName, lastName) {
+  
+    let fullName= firstName + "" + lastName;
+
+function displayFullName(fullName) {
+    alert("Welcome "+ fullName + "!");
+};
+
+displayFullName(fullName);
+};
+ 	welcome('Holberton ', 'School ');
